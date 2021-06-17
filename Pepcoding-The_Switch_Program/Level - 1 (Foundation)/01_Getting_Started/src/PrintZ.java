8c11f3547eeecf67ecd32109d9fdd93d27108cb2
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-06-18 01:43:29 +0530

    Ques1. PrintZ solved

---
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/01_Getting_Started/src/PrintZ.java
@@ -0,0 +1,41 @@
+
+/**
+ * 
+	"The art of programming is nothing but doing the simplest thing that could work!"
+
+	Question:
+	You are required to print a 'z' of size 5 using '*'.
+	
+	Input format:
+	There is no input
+	
+	Output format:
+	
+	*****
+	
+	   *
+	
+	  *
+	
+	*
+	
+	*****
+	
+	Constraints:
+	No Constraints
+	
+ * @author pulkit-rastogi97
+ *
+ */
+
+public class PrintZ {
+
+	public static void main(String[] args) {
+		System.out.println("*****");
+		System.out.println("   * ");
+		System.out.println("  *  ");
+		System.out.println(" *   ");
+		System.out.println("*****");
+	}
+
+}
