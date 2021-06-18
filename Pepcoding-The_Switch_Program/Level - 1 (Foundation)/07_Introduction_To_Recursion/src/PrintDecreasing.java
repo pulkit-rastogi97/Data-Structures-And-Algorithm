d14483b367474b5b2b41b52047e1823e4960da45
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-06-19 01:54:33 +0530

    PrintDecreasing solved

---
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/07_Introduction_To_Recursion/src/PrintDecreasing.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ * 		PRINT DECREASING
+ * 
+  	1. You are given a positive number n. 
+  	2. You are required to print the counting from n to 1.
+	3. You are required to not use any loops. Complete the body of print Decreasing function to achieve it.
+	
+	Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+	Input Format
+	A number n
+	Output Format
+	n
+	n - 1
+	n - 2
+	.. 
+	1
+	Question Video
+	
+	
+	Constraints
+	1 <= n <= 1000
+	Sample Input
+	5
+	Sample Output
+	5
+	4
+	3
+	2
+	1
+
+ * @author pulkit
+ *
+ */
+
+
+public class PrintDecreasing {
+
+	private static void PrintDecreasing(int n) {
+		if(n == 0)
+			return;
+		System.out.println(n);
+		PrintDecreasing(n - 1);
+	}
+	
+	public static void main(String[] args) {
+		int n = 9;
+		PrintDecreasing(n);
+	}
+
+}
