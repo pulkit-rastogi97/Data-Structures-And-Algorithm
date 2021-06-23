a9b15ec45dce685ecd3aa40d6852ca1009ec5b72
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-06-24 02:39:41 +0530

    Helper class added

---
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/10_Recursion_In_Strings/src/Helper.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Helper {
+
+	static Scanner scan = new Scanner(System.in);
+	
+	public static String inputString() {
+		return scan.next();
+	}
+	
+	public static int inputInt() {
+		return scan.nextInt();
+	}
+}
