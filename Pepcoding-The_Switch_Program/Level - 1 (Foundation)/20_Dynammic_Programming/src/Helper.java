fec3b3593948ba34d7d1d159992764b3d39bb10b
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-06-28 03:30:13 +0530

    Method to input integer array of 2 dimensional

--- Pepcoding-The_Switch_Program/Level - 1 (Foundation)/20_Dynammic_Programming/src/Helper.java
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/20_Dynammic_Programming/src/Helper.java
@@ -15,4 +15,13 @@ public class Helper {
 		}
 		return arr;
 	}
+	
+	public static int[][] inputArr(int n, int m){
+		int[][] arr = new int[n][m];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < m; j++)
+				arr[i][j] = scan.nextInt();
+		}
+		return arr;
+	}
 }
