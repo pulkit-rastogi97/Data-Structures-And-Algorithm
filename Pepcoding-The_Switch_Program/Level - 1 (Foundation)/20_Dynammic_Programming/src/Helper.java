48b77da2ccef02633828e586468efcd13f3187bc
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-06-28 01:51:09 +0530

    Method to take integer array of size n as input added.

--- Pepcoding-The_Switch_Program/Level - 1 (Foundation)/20_Dynammic_Programming/src/Helper.java
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/20_Dynammic_Programming/src/Helper.java
@@ -3,8 +3,16 @@ import java.util.Scanner;
 public class Helper {
 
 	static Scanner scan = new Scanner(System.in);
-	
+
 	public static int inputInt() {
 		return scan.nextInt();
+	}
+	
+	public static int[] inputArr(int n) {
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i++) {
+			arr[i] = scan.nextInt();
+		}
+		return arr;
 	}
 }
