85cfb65fa606f11395ab6e6499f00c7da6006091
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-07-14 03:43:18 +0530

    GTHelper.java construct method overloaded

--- Pepcoding-The_Switch_Program/Level - 1 (Foundation)/15_Generic_Tree/src/GTHelper.java
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/15_Generic_Tree/src/GTHelper.java
@@ -53,5 +53,30 @@ public class GTHelper {
 			display(child);
 		}
 	}
+
+	  public static Node construct(int[] arr) {
+	    Node root = null;
+
+	    Stack<Node> st = new Stack<>();
+	    for (int i = 0; i < arr.length; i++) {
+	      if (arr[i] == -1) {
+	        st.pop();
+	      } else {
+	        Node t = new Node();
+	        t.data = arr[i];
+
+	        if (st.size() > 0) {
+	          st.peek().children.add(t);
+	        } else {
+	          root = t;
+	        }
+
+	        st.push(t);
+	      }
+	    }
+
+	    return root;
+	  }
+
 	
 }
