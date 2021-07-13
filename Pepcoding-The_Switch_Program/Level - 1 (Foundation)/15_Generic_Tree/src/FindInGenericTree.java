456e69da0838251f3f19807933c7922d806141a9
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2021-07-14 03:44:14 +0530

    FindInGenericTree solved

---
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/15_Generic_Tree/src/FindInGenericTree.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FindInGenericTree {
+
+	public static boolean find(Node node, int data) {
+		boolean isFound = false;
+		//I will check my data 
+		if(node.data == data)
+			isFound = true;
+		
+		for(Node child : node.children) {
+			isFound = find(child, data);
+			if(isFound == true)
+				return isFound;
+		}
+		return isFound;
+			
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		String[] values = br.readLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(values[i]);
+		}
+
+		int data = Integer.parseInt(br.readLine());
+
+		Node root = GTHelper.construct(arr);
+		boolean flag = find(root, data);
+		System.out.println(flag);
+		// display(root);
+	}
+}
