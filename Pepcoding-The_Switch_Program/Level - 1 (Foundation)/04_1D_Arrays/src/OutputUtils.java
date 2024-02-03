6880c2bdbdaa071bcd8b5788a2cfca8a52c89346
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2024-02-03 18:47:06 +0530

    Added Output Utilities class

---
+++ Pepcoding-The_Switch_Program/Level - 1 (Foundation)/04_1D_Arrays/src/OutputUtils.java
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * @author pulkit-rastogi97
+ *
+ */
+public class OutputUtils {
+
+	public static void printArray(int[] arr) {
+		for(int x: arr) {
+			System.out.print(x + " ");
+		}
+	}
+}
